8632a4c2ebfd1d84b5ad25aef11a982418310080
Ricky Feng <ricky.feng@163.com>
2022-10-27 01:27:39 +0800

    修复样式问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Laravel BBS') - Laravel 进阶教程</title>
-    <!-- 样式 -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+  <meta name="description" content="@yield('description','LaraBBS 爱好者社区。')" />
+  <meta name="keywords" content="@yield('keyword', 'LaraBBS,社区,论坛,开发者论坛')" />
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  @yield('styles')
+
 </head>
+
 <body>
-    <div id="app" class="{{ route_class() }}-page">
-        @include('layouts._header')
-        <div class="container">
-          @include('shared._messages')
-          @yield('content')
-        </div>
-        @include('layouts._footer')
+  <div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+      @include('shared._messages')
+
+      @yield('content')
+
     </div>
-    <!-- JS 脚本 -->
-    <script src="{{ mix('js/app.js') }}"></script>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
+
 </body>
+
 </html>
