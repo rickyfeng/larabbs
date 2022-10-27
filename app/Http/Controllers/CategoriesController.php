df6518bf0829456126286ddc34edf2009719e264
Ricky Feng <ricky.feng@163.com>
2022-10-27 11:14:26 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,11 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
-        $topics = Topic::where('category_id', $category->id)->with('user','category')->paginate(30);
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->paginate(30);
         return view('topics.index', compact('topics', 'category'));
     }
 }
