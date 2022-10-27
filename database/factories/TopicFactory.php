9bcb2503c6e3d173ec6c52c96ed2e90a36378f4b
Ricky Feng <ricky.feng@163.com>
2022-10-27 08:38:31 +0800

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -12,7 +12,11 @@ class TopicFactory extends Factory
     public function definition()
     {
         return [
-            // $this->faker->name,
+            'title'     => $this->faker->sentence(),
+            'body'      => $this->faker->text(),
+            'excerpt'   => $this->faker->sentence(),
+            'user_id'   => $this->faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'category_id' => $this->faker->randomElement([1,2,3,4]),
         ];
     }
 }
