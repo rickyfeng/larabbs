c9f23dda94fd4099c53f400170c4439c9746ce5f
Ricky Feng <ricky.feng@163.com>
2022-11-02 18:03:49 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,17 +3,21 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
-    public function saved(Reply $reply)
-    {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-    }
-
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        $reply->topic->user->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
