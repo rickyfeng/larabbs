9e2e19938b3e8bd3c29c0b52327f74e11f91bd3a
Ricky Feng <ricky.feng@163.com>
2022-11-02 15:24:24 +0800

    回复列表

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
@@ -22,4 +21,5 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class);
     }
+
 }
