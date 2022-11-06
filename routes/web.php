933ddf5a332c31e7419f4ef826105c272a9bd397
Ricky Feng <ricky.feng@163.com>
2022-11-06 17:09:58 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::resource('categories', 'CategoriesController', ['only'=>['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('replies', 'RepliesController', ['only'=>['store', 'destroy']]);
+
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
